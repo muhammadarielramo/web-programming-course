545b52245b9d37932860f3741d3961310b325258
muhammadarielramo <if23.muhammadramo@mhs.ubpkarawang.ac.id>
2024-11-07 22:07:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Halaman Login</h1>
-    <form action="/backend/login.php" method="POST">
-        <input type="email" name="email" id="email" placeholder="Email"/>
-        <input type="password" name="password" id="password" placeholder="Password"/>
-        <button type="submit">Login</button>
+    <h1>login</h1>
+    <form action="./backend/login.php" method="post">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="submit" value="login" name="submit">
     </form>
 
 </body>
