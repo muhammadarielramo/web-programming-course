b732d8b5b6c937f7e7596dedbbf70bb470298f24
muhammadarielramo <if23.muhammadramo@mhs.ubpkarawang.ac.id>
2024-11-08 13:55:22 +0700

    Update show.php

--- show.php
+++ show.php
@@ -3,44 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Produk</title>
+    <!-- Tambahkan Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Data produk</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nama produk</th>
-                <th>harga</th>
-                <th>gambar produk</th>
-                <th>Opsi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                require './config/db.php';
-
-                $products = mysqli_query($db_connect,"SELECT * FROM products");
-                $no = 1;
-
-                while($row = mysqli_fetch_assoc($products)) {
-            ?>
+    <div class="container my-5">
+        <h1 class="mb-4">Data Produk</h1>
+        <table class="table table-striped table-bordered">
+            <thead class="table-dark">
                 <tr>
-                    <td><?=$no++;?></td>
-                    <td><?=$row['name'];?></td>
-                    <td><?=$row['price'];?></td>
-                    <!-- <td><img src="<?=$row['image'];?>" width="100"></td> -->
-                    <td><a href="<?=$row['image'];?>" target="_blank">unduh</a></td>
-                    <td>
-                        <a href="edit.php?id=<?=$row['id'];?>">Edit</a>
-                        <a href="delete.php?id=<?=$row['id'];?>">Hapus</a>
-
-                    </td>
+                    <th>#</th>
+                    <th>Nama Produk</th>
+                    <th>Harga</th>
+                    <th>Gambar Produk</th>
+                    <th>Opsi</th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    
+            </thead>
+            <tbody>
+                <?php
+                    require './config/db.php';
+
+                    $products = mysqli_query($db_connect,"SELECT * FROM products");
+                    $no = 1;
+
+                    while($row = mysqli_fetch_assoc($products)) {
+                ?>
+                    <tr>
+                        <td><?=$no++;?></td>
+                        <td><?=$row['name'];?></td>
+                        <td><?=$row['price'];?></td>
+                        <!-- <td><img src="<?=$row['image'];?>" width="100"></td> -->
+                        <td><a href="<?=$row['image'];?>" class="btn btn-link" target="_blank">Unduh</a></td>
+                        <td>
+                            <a href="edit.php?id=<?=$row['id'];?>" class="btn btn-warning btn-sm">Edit</a>
+                            <a href="delete.php?id=<?=$row['id'];?>" class="btn btn-danger btn-sm">Hapus</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Tambahkan Bootstrap JavaScript dan dependensinya -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
